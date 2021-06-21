c4d52f80930db173534495c09faeee3f1038eb9b
Adedeji Agunbiade <ade@kobo360.com>
2021-06-21 08:20:48 +0100

    initial commit

---
+++ app/js/index.js
@@ -0,0 +1,25 @@
+//testimonial carousel
+// $('.testimonial-carousel').owlCarousel({
+//     loop:false,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:3,
+//             nav:true,
+//             loop:true
+//         }
+//     }
+// })
+
+AOS.init({ easing: "ease" });
+
+const simpleParallax = require("simple-parallax-js");
